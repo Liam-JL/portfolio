59f6c634e53442d5d692ba31888c7836b52f51e9
LiamJL <paledeepened@gmail.com>
2025-10-22 18:03:00 +0100

    Add CTA button component to Header and implement all Header styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,27 @@
 import styles from './Header.module.css';
 import Logo from '../Logo/Logo';
+import Button from '../Button/Button';
+import { MdMenuOpen } from "react-icons/md";
 
 
 function Header() {
     return (
         <header className={styles.header}>
-            <div className={`${styles.block}`}>
+            <div className={`${styles.block} ${styles.logoBlock}`}>
                 <Logo />
             </div>
 
-            <div className={`${styles.block}`}>
-                
+            <div className={`${styles.block} ${styles.navBlock}`}>
+                {/* Nav to be implemented in future version */}
             </div>
             
-            <div className={`${styles.block}`}>
-                
+            <div className={`${styles.block} ${styles.ctaBlock}`}>
+                <Button variant='primary' size='lg'><p>Email Me</p></Button>
             </div>
 
-
-
+            <div className={`${styles.block} ${styles.openSideMenuBlock}`}>
+                <MdMenuOpen  className={styles.menuIcon}/>
+            </div>
         </header>
     )
 }
