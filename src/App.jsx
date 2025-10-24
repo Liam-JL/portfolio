85ee6118cfdf1c287bfbb85cd4d8b5937ae7981e
LiamJL <paledeepened@gmail.com>
2025-10-24 08:29:43 +0100

    Implement scaffolding for main contents

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import Header from "./components/Header/Header"
 import Hero from "./components/Hero/Hero"
+import Contents from "./components/Contents/Contents"
 
 function App() {
   
 
   return (
-    <>
+    <div className="App">
       <Header />
       <Hero />
-    </>
+      <Contents />
+    </div>
   )
 }
 
--- src/components/Contents/Contents.jsx
+++ src/components/Contents/Contents.jsx
@@ -1,11 +1,24 @@
+import styles from './Contents.module.css';
 
 function Contents() {
   
-
   return (
-    <>
+    <main className={styles.contentContainer}>
+      <div className={styles.content}>
+        <section className={`${styles.block} ${styles.stackBlock}`}>
+          <h3 className={`${styles.blockHeader} ${styles.stackHeader}`}>Stack</h3>
+          <ul className={styles.stackList}>
 
-    </>
+          </ul>
+        </section>
+        <section className={`${styles.block} ${styles.projectsBlock}`}>
+          <h3 className={`${styles.blockHeader} ${styles.projectsHeader}`}>Projects</h3>
+          <ul className={styles.projectsList}>
+
+          </ul>
+        </section>
+      </div>
+    </main>
   )
 }
 
